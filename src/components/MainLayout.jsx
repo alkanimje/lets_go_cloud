e927dd0f494d193f73d22b66a2b7c5cbffa1c7b8
Kajrolkar <jyotshana.kajrolkar@capgemini.com>
2023-09-14 17:22:13 -0500

    jyo changes

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+
 import Header from './Header';
+import Footer from './Footer';
+//import ReactDOM from 'react-dom';
+import Checkbox from 'rc-checkbox';
+//import 'rc-checkbox/assets/index.css';
+
+ 
 
 export default function main(){
+ //   render() {
     return (
-        <><Header />
-            <div>
-                <p>Main Content</p>
-            </div>
-        </>
-
-        /// <Footer>
+    <div>
+        <Header/>
+        <Checkbox value="S3"> S3 Bucket</Checkbox>
+        <Footer/>
+    </div>
     );
 
+ 
+    
 }
