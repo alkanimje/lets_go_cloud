dde36cc6298b8bc5921bc9d282caa1fd2c32af03
Nimje <alka.nimje@capgemini.com>
2023-09-14 17:13:09 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mainlayout from "./components/MainLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <Mainlayout/>
+    </div>
+  );
+}
+
+export default App;
